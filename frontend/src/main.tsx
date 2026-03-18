2f4245a0db1639029b164c8372cc718acf18e347
ceenen <phunh.work@gmail.com>
2026-03-18 20:47:52 +0700

    [Fix] Minor changes in frontend - search engine in HomePage

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import { UniversityProvider } from './context/UniversityContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <UniversityProvider>
+        <App />
+      </UniversityProvider>
     </BrowserRouter>
   </StrictMode>,
 )
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,4 +10,12 @@ export default defineConfig({
     tailwindcss(),
     svgr(),
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
